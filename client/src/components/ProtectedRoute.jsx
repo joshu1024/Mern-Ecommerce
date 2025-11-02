190e83fded417db189299a79365bf4f335950a98
josh1024 <joshuakipamet@gmail.com>
2025-11-02 12:22:12 -0800

    Updating ProductDetails

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -5,12 +5,10 @@ const ProtectedRoute = ({ children }) => {
   const { isLoggedIn } = useSelector((state) => state.user);
   const location = useLocation();
 
-  // If not logged in, redirect to /login
   if (!isLoggedIn) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  // otherwise, allow access
   return children;
 };
 
