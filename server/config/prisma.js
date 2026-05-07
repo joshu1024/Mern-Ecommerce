e0e07f78d99144b7e297b9113eacc030ada30a9c
josh1024 <joshuakipamet@gmail.com>
2026-05-07 10:28:44 +0300

    refactor -adding dotenv to prisma.config

--- server/config/prisma.js
+++ server/config/prisma.js
@@ -1,8 +1,10 @@
-import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -1,6 +1,8 @@
-import "dotenv/config";
 import { defineConfig } from "prisma/config";
+import "dotenv/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+   datasource: {
+    url: process.env.DATABASE_URL,
+  },
 });
